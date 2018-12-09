5b4c7d280b5b79052b1e36f16ee47237c885d6a0
rishivamshi <rishivamshi0079@gmail.com>
2018-12-09 15:56:11 +0530

    added comments

--- blockchain.js
+++ blockchain.js
@@ -1,8 +1,9 @@
+// simple hashing algorithm - just adds * to the end.
 const simpleHash = (data) => {
   return data + '*';
 }
 
-
+// Block class to store data, hash and hash of the last block - lastHash (creates a link)
 class Block {
   constructor (data, hash, lastHash) {
     this.data = data;
@@ -11,6 +12,7 @@ class Block {
   }
 }
 
+// Blockchain class to create a genesis block and an array of all the blocks.
 class Blockchain {
   constructor () {
     const genesis = new Block('gen-data', 'gen-hash', 'gen-lastHash');
@@ -26,10 +28,11 @@ class Blockchain {
 
 }
 
+// testing the implementation
 const fooBlock = new Blockchain();
 fooBlock.addBlock('one');
 fooBlock.addBlock('two');
 fooBlock.addBlock('three');
 
-
+// printing into console - node Blockchain.js
 console.log(fooBlock);
